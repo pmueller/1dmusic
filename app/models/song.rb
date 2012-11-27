9da8f73af8ff10e094e03f0bc5bea611964a5593
Preston Mueller <preston.mueller@techshido.com>
2012-11-27 14:24:44 -0500

    Migrated db and annotated

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  key        :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Song < ActiveRecord::Base
   attr_accessible :key, :title
 end
