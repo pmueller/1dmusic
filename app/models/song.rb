71a6406c0696caccdf4fbe87e3d3c5fe14fb3687
Preston Mueller <preston.mueller@techshido.com>
2012-11-27 14:23:33 -0500

    Initial commit, new rails project with song model

---
+++ app/models/song.rb
@@ -0,0 +1,3 @@
+class Song < ActiveRecord::Base
+  attr_accessible :key, :title
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewMusic::Application.config.secret_token = '69c570c3197bf7f3ceedabb6a52790bba855b7440df9a8ee8fb1d06cb55cccdb3244a7149fa7f7481e3ecbdee37cd7f4cdf76e7fac55dfbfed7831cf6e2bcd18'
