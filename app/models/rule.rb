09fdf4df681a7050553c91a627a980f493b41596
Preston Mueller <preston.mueller@techshido.com>
2012-11-28 00:14:08 -0500

    Added the match method so rules are useful

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -14,4 +14,16 @@ class Rule < ActiveRecord::Base
   belongs_to :song
 
   attr_accessible :new_cell, :to_match
+
+  def match(current_gen, i)
+    ret = false
+
+    if (i > 0 && i < 11)
+      if current_gen[i-1] == to_match[0] && current_gen[i] == to_match[1] && current_gen[i+1] == to_match[2]
+        ret = true
+      end
+    end
+
+    ret
+  end
 end
